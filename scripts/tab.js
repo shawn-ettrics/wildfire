1846f6202256d7ba04a6fe4de2f586becb31d2ed
shawn-ettrics <shawn@ettrics.com>
2023-03-07 13:17:22 -0800

    code refactor

--- scripts/tab.js
+++ scripts/tab.js
@@ -9,7 +9,6 @@ const resizeObserver = new ResizeObserver(entries => {
 })
 
 
-
 tabCards.forEach(c => {
     c.style.padding = '0'
     resizeObserver.observe(c)
@@ -18,19 +17,18 @@ tabCards.forEach(c => {
 
 const tabObjArr = []
 
-vertTabs.forEach((item, i) => {
-    let nextPls
-    tabObjArr.push({
-        container: item,
-        indicator: item.querySelector('.tab-vertical-active-indicator'),
-        tabs: item.querySelectorAll('.tab'),
+function createVerticalTabs(elm, index) {
+    return {
+        container: elm,
+        indicator: elm.querySelector('.tab-vertical-active-indicator'),
+        tabs: elm.querySelectorAll('.tab'),
         activeIndex: 0,
-        frame: tabCards[i].querySelector('.frame'),
-        imgs: tabCards[i].querySelectorAll('img'),
-        srcs: [...tabCards[i].querySelectorAll('img')].map(img => {
+        timeoutFunc: null,
+        frame: tabCards[index].querySelector('.frame'),
+        imgs: tabCards[index].querySelectorAll('img'),
+        srcs: [...tabCards[index].querySelectorAll('img')].map(img => {
             return img.src
         }),
-
         moveIndicator() {
             this.indicator.style.opacity = '0.3'
             this.indicator.style.height = '0.5em'
@@ -103,8 +101,6 @@ vertTabs.forEach((item, i) => {
                 }
             })
         },
-
-
         tabClickHandler(clickedIndex) {
             clearTimeout(nextPls)
             this.activate(clickedIndex)
@@ -117,9 +113,119 @@ vertTabs.forEach((item, i) => {
             }, 4000);
         },
         stopPlaying() {
-            clearTimeout(nextPls)
+            clearTimeout(this.timeoutFunc)
         }
-    })
+        
+    }
+}
+
+
+vertTabs.forEach((item, i) => {
+
+    const tabObj = createVerticalTabs(item, i)
+    tabObjArr.push(tabObj)
+
+    // let nextPls
+    // tabObjArr.push({
+    //     container: item,
+    //     indicator: item.querySelector('.tab-vertical-active-indicator'),
+    //     tabs: item.querySelectorAll('.tab'),
+    //     activeIndex: 0,
+    //     frame: tabCards[i].querySelector('.frame'),
+    //     imgs: tabCards[i].querySelectorAll('img'),
+    //     srcs: [...tabCards[i].querySelectorAll('img')].map(img => {
+    //         return img.src
+    //     }),
+
+    //     moveIndicator() {
+    //         this.indicator.style.opacity = '0.3'
+    //         this.indicator.style.height = '0.5em'
+    //         setTimeout(() => {
+    //             this.indicator.style.opacity = '1'
+    //             this.indicator.style.height = '1.5em'
+    //             let pos = getTabPos(this.tabs[this.activeIndex].querySelector('.tab-number'), this.container)
+    //             this.indicator.style.bottom = `calc(${pos}px + 0.2em)`
+    //         }, 600);
+    //     },
+
+    //     prepare() {
+
+    //         this.tabs.forEach((tab, i) => {
+    //             tab.addEventListener('click', ()=>{
+    //                 this.tabClickHandler(i)
+    //             })
+
+    //             let tabImg = document.createElement('img')
+    //             tabImg.src = this.srcs[i]
+
+    //             tabImg.onclick = () => {
+
+    //             }
+
+    //             tab.append(tabImg)
+
+    //         })
+    //         this.activate(this.activeIndex)
+    //         // this.hoverHandler(this.frame)
+    //         this.frame.onmouseover = () => {
+    //             this.stopPlaying()
+    //         }
+    //         this.frame.onmouseout = () => {
+    //             this.autoPlay()
+    //         }
+    //     },
+
+    //     activate(index) {
+    //         this.tabs.forEach((tab, i) => {
+    //             const img = tab.querySelector('img')
+    //             img.onmouseover = () => {
+    //                 this.stopPlaying()
+    //             }
+    //             img.onmouseout = () => {
+    //                 this.autoPlay()
+    //             }
+    //             if (i===index) {
+    //                 tab.classList.add('active')
+    //                 resizeObserver.observe(img)
+    //             } else {
+    //                 tab.classList.remove('active')
+    //                 resizeObserver.unobserve(img)
+    //                 img.style.height = 0
+    //             }
+    //         })
+    //         this.activeIndex = index
+    //         this.moveIndicator()
+
+    //         this.frame.classList.remove('active')
+    //         void this.frame.offsetWidth
+    //         this.frame.classList.add('active')
+    //         this.imgs.forEach((img, i) => {
+    //             if (i === index) {
+    //                 img.classList.add('active')
+
+    //             } else {
+    //                 img.classList.remove('active')
+                    
+    //             }
+    //         })
+    //     },
+
+
+    //     tabClickHandler(clickedIndex) {
+    //         clearTimeout(nextPls)
+    //         this.activate(clickedIndex)
+    //     },
+    //     autoPlay() {
+    //         nextPls = setTimeout(() => {
+    //             this.activeIndex = (this.activeIndex >= this.tabs.length - 1)? 0 : this.activeIndex + 1
+    //             this.tabs[this.activeIndex].click()
+    //             this.autoPlay()
+    //         }, 4000);
+    //     },
+    //     stopPlaying() {
+    //         clearTimeout(nextPls)
+    //     }
+    // })
 })
 
 tabObjArr.forEach(obj => {
