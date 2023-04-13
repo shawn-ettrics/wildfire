e27e161bfa456502ed180809c57f5673e231ce51
shawn-ettrics <shawn@ettrics.com>
2023-04-13 15:45:46 -0700

    progresstimer

--- scripts/horizontalTabs.js
+++ scripts/horizontalTabs.js
@@ -1,9 +1,9 @@
-const timeInterval = 4
+const tabDuration = 4000
 const mobileBreakpoint = 767
 
 const tabMenus = document.querySelectorAll('.w-tab-menu')
 
-document.querySelector(':root').style.setProperty('--time', `${timeInterval}s`)
+document.querySelector(':root').style.setProperty('--time', `${tabDuration}s`)
 
 tabMenus.forEach( menu => {
 
@@ -17,6 +17,9 @@ tabMenus.forEach( menu => {
 
     tabLinks.forEach( (tab, i) => {
 
+        const progressbar = document.createElement('div')
+        progressbar.classList.add('progressbar')
+        tab.append(progressbar)
         tab.dataset.tabIndex = i
         if (tab.classList.contains('w--current')) {
             activateNext(tab)
@@ -28,12 +31,30 @@ tabMenus.forEach( menu => {
     
     function activateNext(currentTab) {
         let currentIndex = parseInt(currentTab.dataset.tabIndex)
-        let nextIndex = currentIndex >= tabLinks.length - 1? 0 : currentIndex + 1 
-        setTimeout( () => {
-            tabLinks[nextIndex].click()
-            let scrollAmount = tabLinks[nextIndex].offsetLeft
-            currentTab.parentElement.scrollTo({left: scrollAmount, behavior: 'smooth'})
-        }, timeInterval * 1000)
+        let nextIndex = currentIndex >= tabLinks.length - 1? 0 : currentIndex + 1
+        let remainingDuration = tabDuration
+        const activeBar = currentTab.querySelector('.progressbar')
+
+        const progressTimer = () => {
+            activeBar.style.width = `${(tabDuration - remainingDuration) / tabDuration * 100}%`
+            remainingDuration = remainingDuration - 100
+            if (remainingDuration > 0) {
+                setTimeout(() => {
+                    progressTimer()
+                }, 100)
+            } else {
+                tabLinks[nextIndex].click()
+                let scrollAmount = tabLinks[nextIndex].offsetLeft
+                currentTab.parentElement.scrollTo({left: scrollAmount, behavior: 'smooth'})
+            }
+        }
+        progressTimer()
+
+        // setTimeout( () => {
+        //     tabLinks[nextIndex].click()
+        //     let scrollAmount = tabLinks[nextIndex].offsetLeft
+        //     currentTab.parentElement.scrollTo({left: scrollAmount, behavior: 'smooth'})
+        // }, tabDuration)
     }
     // tabLinks[1].click()
 
